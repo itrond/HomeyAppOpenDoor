4ec2ecf5e475f0c72863ebb4d33a2b6145f087ba
Trond Aarø <trond@itrond.net>
2019-04-07 00:48:00 +0200

    implemented logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section class="section">
+    <div class="container">
+      <p class="subtitle">Enter code to unlock door</p>
+      <div class="field">
+        <div class="control">
+          <input v-model="theCode" class="input is-large" type="number" placeholder="Enter code">
+        </div>
+      </div>
+
+      <a
+        v-on:click="unlock"
+        class="button is-success is-large"
+        v-bind:class="{ 'is-loading': isUnlocking}"
+      >Lås opp</a>
+      <br>
+      <br>
+      <a class="button is-warning is-large" v-on:click="lock">Lås</a>
+    </div>
+  </section>
 </template>
 
 <script>
+import { setTimeout } from "timers";
+
+const baseUrl =
+  `https://${process.env.VUE_APP_HOMEY_CLOUD_ID}.` +
+  `${process.env.VUE_APP_HOMEY_LOGIC_WEBHOOK}`;
+
+const unlockEvent = process.env.VUE_APP_UNLOCK_EVENT;
+const lockEvent = process.env.VUE_APP_LOCK_EVENT;
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  mounted: () => {
+    console.log(process.env);
+  },
+  data: () => ({
+    isUnlocking: false,
+    isLocking: false,
+    theCode: ""
+  }),
+  methods: {
+    unlock: function() {
+      this.isUnlocking = true;
+      fetch(`${baseUrl}/${unlockEvent}?tag=${this.theCode}`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(JSON.stringify(myJson));
+        });
+      this.theCode = "";
+      setTimeout(() => (this.isUnlocking = false), 1000);
+    },
+    lock: function() {
+      this.isLocking = true;
+      fetch(`${baseUrl}/${lockEvent}`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(JSON.stringify(myJson));
+        });
+      setTimeout(() => (this.isLocking = false), 1000);
+    }
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
